7e3c64fe40a4ad96964d29868a763c0c7c552b72
Lucileide-Elpidio <luhelpidio23@outlook.com>
2023-08-24 21:02:32 -0300

    projeto finalizado

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import * as Yup from "yup";
 import User from "../models/User";
+import jwt from "jsonwebtoken";
+import authConfig from "../../config/auth"
 
 class SessionController {
     async store(request, response) {
@@ -9,10 +11,9 @@ class SessionController {
             password: Yup.string().required(),
         })
 
-        if (await schema.isValid(request.body)){
-            return response.status(400).json({ error: "Make sure your password or email are " })
+        if (!(await schema.isValid(request.body))) {
+            return response.status(400).json({ error: "Make sure your password or email are correct " })
         }
-        
 
         const { email, password } = request.body
 
@@ -21,22 +22,27 @@ class SessionController {
         })
 
 
-        if (user){
+        if (!user) {
             return response.status(400).json({ error: "Make sure your password or email are correct" })
         }
-        
+
 
         if (!(await user.checkPassword(password))) {
             return response.status(400).json({ error: "Make sure your password or email are correct" })
         }
-        
+
 
         return response.json(
             {
                 id: user.id,
                 email,
                 name: user.name,
-                admin: user.admin
+                admin: user.admin,
+                token: jwt.sign({ id: user.id, name:user.name }, authConfig.secret,
+                    {
+                        expiresIn: authConfig.expiresIn
+                    }
+                )
             })
 
     }
